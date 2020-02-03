80198507452fcd0d23e7ae22fe0c82da57551df5
kuraram <p232034m@mail.kyutech.jp>
2020-02-03 16:52:05 +0900

    implement initialization

--- cGA/genetic.rb
+++ cGA/genetic.rb
@@ -2,20 +2,39 @@
 
 require "./individual.rb"
 require "./global.rb"
+require "./randomnumber"
 
 
 class Genetic
-    attr_accessor :seed, :individuals, :p
+	attr_accessor :seed, :individuals, :p
 
-    def initialize(seed)
-        @seed = seed    # seed値
-        @individuals = []   # 全固体を格納
-        @p = [] # Probabilityを格納
-    end
+	def initialize(seed)
+		@seed = seed    # seed値
+		@individuals = []   # 全固体を格納
+		@p = [] # Probabilityを格納
+	end
 
-    def Genetic()
+	def Genetic()
 
-    end
+		Initialization()
+
+	end
+
+	def Initialization()
+		
+		for i in 1..INDIVIDUAL_NUM do	# 第１世代の個体を生成
+			ind = Individual.new
+			for j in 0..(GENE_NUM-1) do
+				ind.chrom[j] = (1.999999 * RandomNumber()).floor;
+			end
+			@individuals << ind
+		end
+		
+		for i in 0..(INDIVIDUAL_NUM-1) do	# 初期確率ベクトルの生成
+			@p[i] = 0.5
+		end
+		
+	end
 
 end
 
---
+++ cGA/randomnumber.rb
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def RandomNumber
+    return rand(0.0..1.0)
+end
